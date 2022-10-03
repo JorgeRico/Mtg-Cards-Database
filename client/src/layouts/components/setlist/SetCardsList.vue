b9f35d8da636fda75f4ea742f7a16a63c5c19177
Jorge Rico <jordir@vdmhealth.com>
2022-10-04 01:28:28 +0200

    feature: check uncheck cards

--- client/src/layouts/components/setlist/SetCardsList.vue
+++ client/src/layouts/components/setlist/SetCardsList.vue
@@ -2,7 +2,7 @@
     <div id="setResult" class="mt-10 w-100">
         <div class="left w-100">
             <v-btn color="primary" class="right me-3 mb-5">
-                <span class="d-none d-sm-block" @click="completeAll">Complete all</span>
+                <span class="d-none d-sm-block" @click="setAllCards">Complete all</span>
             </v-btn>
         </div>
         <div class="left w-100">
@@ -41,7 +41,9 @@
                             </td>
                             <td class="text-uppercase w-40">
                                 <p class="mb-0 center" cols="6">
-                                    {{ item.own }}
+                                    <span v-if="item.own==0" @click="setOwnYes(item.id)" class="pointer"><u>update to
+                                            YES</u></span>
+                                    <span v-else @click="setOwnNo(item.id)" class="pointer"><u>update to NO</u></span>
                                 </p>
                             </td>
                         </tr>
@@ -49,20 +51,32 @@
                 </template>
             </v-simple-table>
         </div>
+        <ApiError></ApiError>
     </div>
 </template>
+
+<style>
+.pointer {
+    cursor: pointer;
+}
+</style>
 
 <script>
 import axios from "axios";
 import qs from 'qs';
+import ApiError from '@/layouts/components/ApiError.vue'
 
 export default {
     components: {
-
+        ApiError
     },
     data() {
         return {
             setId: null,
+            // set: null,
+            complete: null,
+            setTotalCards: null,
+            numOwnedCards: 0,
         }
     },
     setup() {
@@ -70,42 +84,166 @@ export default {
         }
     },
     props: {
+        set: {
+            type: Object,
+            default: null,
+        },
         cardsList: {
             type: Array,
             default: [],
         },
     },
     methods: {
-        completeAll() {
-            let text = "Are you sure?!\nEither OK or Cancel.";
-            if (confirm(text) == true) {
-                text = true;
-            } else {
-                text = false;
-            }
-            console.log(text)
-            this.setComplete();
-        },
-
-        async setComplete() {
-            this.urlSetCardsList = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_CARDS_ENDPOINT + '/' + this.setId + '/cards';
+        async setOwnYes(id) {
+            var urlSetCardYes = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_CARDS_ENDPOINT + '/' + this.setId + '/cards/' + id;
 
             await axios({
                 method: 'put',
-                url: this.urlSetCardsList,
+                url: urlSetCardYes,
                 headers: { 'content-type': 'application/x-www-form-urlencoded' },
                 data: qs.stringify({ 'own': '1' }),
             })
                 .then(response => {
-                    // this.cardsList = response.data.data;
-                    console.log('ok')
+                    this.getSetInfoToCheckNumCardsReload();
                 })
                 .catch(error => {
                     console.log('error')
-                    // this.show('errorApiFile');
-                    // setTimeout(() => this.hide('errorApiFile'), 2500);
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
                 })
                 .finally(() => this.loading = false)
+
+        },
+        async setOwnNo(id) {
+            var urlSetCardYes = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_CARDS_ENDPOINT + '/' + this.setId + '/cards/' + id;
+
+            await axios({
+                method: 'put',
+                url: urlSetCardYes,
+                headers: { 'content-type': 'application/x-www-form-urlencoded' },
+                data: qs.stringify({ 'own': '0' }),
+            })
+                .then(response => {
+                    console.log(this.complete)
+                    this.getSetInfoToCheckCompleteReload();
+                })
+                .catch(error => {
+                    console.log('error')
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
+                })
+                .finally(() => this.loading = false)
+        },
+        completeAll() {
+            let text = "Are you sure?!\nEither OK or Cancel.";
+            if (confirm(text) == true) {
+                text = true;
+                this.setAllCards();
+            } else {
+                text = false;
+            }
+        },
+
+        async setAllCards() {
+            var urlSetAllCards = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_CARDS_ENDPOINT + '/' + this.setId + '/cards';
+
+            await axios({
+                method: 'put',
+                url: urlSetAllCards,
+                headers: { 'content-type': 'application/x-www-form-urlencoded' },
+                data: qs.stringify({ 'own': '1' }),
+            })
+                .then(response => {
+                    this.setComplete('1');
+                })
+                .catch(error => {
+                    console.log('error')
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
+                })
+                .finally(() => this.loading = false)
+        },
+        async setComplete(value) {
+            var urlSetComplete = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_ENDPOINT + '/' + this.setId;
+
+            await axios({
+                method: 'put',
+                url: urlSetComplete,
+                headers: { 'content-type': 'application/x-www-form-urlencoded' },
+                data: qs.stringify({ 'complete': value }),
+            })
+                .then(response => {
+                    location.reload();
+                })
+                .catch(error => {
+                    console.log('error')
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
+                })
+                .finally(() => this.loading = false)
+        },
+        // setSetInfo() {
+        //     this.complete = this.set.complete;
+        //     this.setTotalCards = this.set.setTotalCards;
+        // },
+        getNumCards() {
+            this.numOwnedCards = 0;
+            for (var i = 0; i < this.cardsList.length; i++) {
+                if (this.cardsList[i].own == 1) {
+                    this.numOwnedCards++;
+                }
+            }
+        },
+        async getSetInfoToCheckCompleteReload() {
+            var urlSet = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_ENDPOINT + '/' + this.setId;
+
+            await axios
+                .get(urlSet)
+                .then(response => {
+                    var set = response.data.data;
+                    if (set[0].complete == 1) {
+                        this.setComplete('0');
+                    } else {
+                        location.reload();
+                    }
+                })
+                .catch(error => {
+                    console.log('error')
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
+                })
+                .finally(() => this.loading = false)
+        },
+        async getSetInfoToCheckNumCardsReload() {
+            var urlSet = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_ENDPOINT + '/' + this.setId;
+
+            await axios
+                .get(urlSet)
+                .then(response => {
+                    var set = response.data.data;
+                    this.getNumCards()
+                    if (set[0].setTotalCards == parseInt(this.numOwnedCards) + 1) {
+                        this.setComplete('1');
+                    } else {
+                        location.reload();
+                    }
+                })
+                .catch(error => {
+                    console.log('error')
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
+                })
+                .finally(() => this.loading = false)
+        },
+        show(id) {
+            var element = document.getElementById(id);
+            element.classList.remove("invisible");
+            element.classList.add("visible");
+        },
+        hide(id) {
+            var element = document.getElementById(id);
+            element.classList.remove("visible");
+            element.classList.add("invisible");
         },
     },
     beforeMount() {
--- client/src/views/pages/setlist/SetCards.vue
+++ client/src/views/pages/setlist/SetCards.vue
@@ -4,6 +4,7 @@
             <div cols="12" class="w-100">
                 <h1>MTG Cards</h1>
                 <SetCardsList :cardsList="cardsList"></SetCardsList>
+                <ApiError></ApiError>
             </div>
         </v-card-text>
     </v-card>
@@ -11,9 +12,8 @@
   
 <script>
 import SetCardsList from '@/layouts/components/setlist/SetCardsList.vue';
+import ApiError from '@/layouts/components/ApiError.vue'
 import axios from "axios";
-
-
 
 import { getAuth } from "firebase/auth";
 const auth = getAuth();
@@ -21,16 +21,20 @@ const auth = getAuth();
 export default {
     components: {
         SetCardsList,
+        ApiError,
     },
     data() {
         return {
             setId: null,
             cardsList: [],
             urlSetCardsList: null,
+            set: null,
         }
     },
     methods: {
         async getSetList() {
+            this.urlSetCardsList = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_CARDS_ENDPOINT + '/' + this.setId;
+
             await axios
                 .get(this.urlSetCardsList)
                 .then(response => {
@@ -38,18 +42,28 @@ export default {
                 })
                 .catch(error => {
                     console.log('error')
-                    // this.show('errorApiFile');
-                    // setTimeout(() => this.hide('errorApiFile'), 2500);
+                    this.show('errorApiFile');
+                    setTimeout(() => this.hide('errorApiFile'), 2500);
                 })
                 .finally(() => this.loading = false)
         },
+        show(id) {
+            var element = document.getElementById(id);
+            element.classList.remove("invisible");
+            element.classList.add("visible");
+        },
+        hide(id) {
+            var element = document.getElementById(id);
+            element.classList.remove("visible");
+            element.classList.add("invisible");
+        },
+
     },
     mounted() {
 
     },
     beforeMount() {
         this.setId = this.$route.params.id;
-        this.urlSetCardsList = process.env.VUE_APP_API_SERVER + process.env.VUE_APP_API_SET_CARDS_ENDPOINT + '/' + this.setId;
         this.getSetList();
 
         auth.onAuthStateChanged(function (user) {
