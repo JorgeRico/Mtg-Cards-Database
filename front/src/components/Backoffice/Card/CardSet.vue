d00402d49b35ad5d6fac43638b17eefc9a6aaf08
JorgeRico <jorge.rico.garriga@gmail.com>
2025-05-06 18:14:16 +0200

    set cards route

--- front/src/components/Backoffice/Card/CardSet.vue
+++ front/src/components/Backoffice/Card/CardSet.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
     import BackofficeLayout from '@layouts/BackofficeLayout.vue';
+    import { useRoute } from 'vue-router'
+    import { ref } from 'vue'
 
+    const route = useRoute()
+    const id    = ref(route.params.id)
 </script>
 
 <template>
     <BackofficeLayout>
         <h1>card set template</h1>
-        <p>Looking for Product: {{ $route.params.id }}</p>
-
+        <p>Looking for Product: {{ id }}</p>
     </BackofficeLayout>
 </template>
 
