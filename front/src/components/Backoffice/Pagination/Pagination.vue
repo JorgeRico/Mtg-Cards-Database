efc89e4500b2e0a415877dc4e512c8d0c2b32989
JorgeRico <jorge.rico.garriga@gmail.com>
2025-05-11 01:10:30 +0200

    hotfix pagination

--- front/src/components/Backoffice/Pagination/Pagination.vue
+++ front/src/components/Backoffice/Pagination/Pagination.vue
@@ -11,22 +11,30 @@
     
     const handleChange = (index: number) => {
         emit('customPageChange', index)
-    }
+    }    
 </script>
 
 <template>
     <div class="left mt10 w100">
         <nav aria-label="Page navigation example">
             <ul class="pagination justify-content-end">
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                <li v-if="props.page == 1" class="page-item disabled">
+                    <a class="page-link">Previous</a>
                 </li>
+                <li v-else class="page-item pointer">
+                    <a class="page-link pointer" @click="handleChange(props.page-1)">Previous</a>
+                </li>
+
                 <li class="page-item" v-for="index in props.total" :key=index>
                     <a v-if="props.page==index" class="active page-link pointer" @click="handleChange(index)">{{ index }}</a>
                     <a v-else class="page-link pointer" @click="handleChange(index)">{{ index }}</a>
                 </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
+
+                <li v-if="props.total > props.page" class="page-item">
+                    <a class="page-link pointer" @click="handleChange(props.page+1)">Next</a>
+                </li>
+                <li v-else class="page-item disabled">
+                    <a class="page-link">Next</a>
                 </li>
             </ul>
         </nav>
