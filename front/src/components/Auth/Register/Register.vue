e1b6335c1705d0e3422d15c3b3c11d88055951cc
JorgeRico <jorge.rico.garriga@gmail.com>
2025-05-14 00:39:25 +0200

    register firebase form

--- front/src/components/Auth/Register/Register.vue
+++ front/src/components/Auth/Register/Register.vue
@@ -36,23 +36,13 @@
                     <h3 class="mb-4">Register</h3>
                 </div>
             </div>
-            <!-- <form action="#" class="signin-form"> -->
-                <!-- <div class="form-group mb-3">
-                    <label class="label" for="name">Username</label>
-                    <input type="text" class="form-control mt5" placeholder="Username" required>
-                </div>
-                <div class="form-group mb-3">
-                    <label class="label" for="password">Password</label>
-                <input type="password" class="form-control mt5" placeholder="Password" required> -->
-                <dynamic-form :form=form @submit.prevent="checkRegister" class="signin-form">
-                    <input :label="form.fields.email.label" class="form-control mb-3">
-                    <input :label="form.fields.password.label" class="form-control mb-3">
-                </dynamic-form>
-                <!-- </div> -->
-                <div class="form-group">
-                    <button type="submit" class="form-control btn-auth submit px-3 mt10" @click="checkRegister">Register</button>
-                </div>
-            <!-- </form> -->
+            <dynamic-form :form=form @submit.prevent="checkRegister" class="signin-form">
+                <input :label="form.fields.email.label" class="form-control mb-3">
+                <input :label="form.fields.password.label" class="form-control mb-3">
+            </dynamic-form>
+            <div class="form-group">
+                <button type="submit" class="form-control btn-auth submit px-3 mt10" @click="checkRegister">Register</button>
+            </div>
         </div>
     </AuthLayout>
 </template>
